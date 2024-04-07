be077557ab3cb4c2a5ccc46843ca1b6d9b96e74d
DESKTOP-RQTRSJ3\Administrator <jenbh13@gmail.com>
2024-04-07 16:05:26 +0200

    Test.jsx creacion items pruebas de framermotion

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Navbar from "./components/navbar/NavBar";
 
 const App = () => {
   return <div>
-    {/*<section>
-      <Navbar/>
+    <section>
+      <Navbar />
     </section>
     <section>Parallax</section>
     <section>Service</section>
@@ -13,10 +13,11 @@ const App = () => {
     <section>Portofolio1</section>
     <section>Portofolio2</section>
     <section>Portofolio3</section>
-    <section>Contac</section>*/}
-    <Test/>
-    </div>;
-    
+    <section>Contac</section>
+    {/*<Test/>
+    <Test/>*/}
+  </div>;
+
 };
 
 export default App;
--- src/Test.jsx
+++ src/Test.jsx
@@ -1,11 +1,34 @@
-import {motion} from "framer-motion"
+import {motion} from "framer-motion";
+import {useState } from "react";
 
 const Test = () => {
+
+
+    const [open, setOpen] = useState(false)
+
+
+    const variants = {
+        visible: (i) =>( {
+            opacity:1, 
+            x:100, 
+            transition:{ delay:i * 0.3 },
+        }),
+        hidden: { opacity: 0 },
+    };
+
+    const items = ["item1","item2","item3","item4"];
+
     return (
         <div className="course">
-            <motion.div className="box" animate={{opacity: 0}} transition={{duration: 2}}></motion.div>
+           <motion.ul initial ="hidden" animate= "visible" variants={variants}>
+            {items.map((item,i) => (
+              <motion.li variants={variants} key={item} custom= {i}>
+                {item} 
+              </motion.li>
+            ))}
+           </motion.ul> 
         </div>
-    )
-}
+    );
+};
 
-export default Test
+export default Test;
