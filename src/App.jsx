4c9013a2158d2a403dff5ff36f1d78182350641d
DESKTOP-RQTRSJ3\Administrator <jenbh13@gmail.com>
2024-04-17 08:45:10 +0200

    creacion de Parallax y cambios imagen

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import Test from "./Test"
+import Test from "./Test";
 import "./app.scss"
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/NavBar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return <div>
-    <section>
+    <section id="Homepage">
       <Navbar />
       <Hero />
     </section>
-    <section>Parallax</section>
-    <section>Service</section>
-    <section>Parallax</section>
-    <section>Portofolio1</section>
-    <section>Portofolio2</section>
-    <section>Portofolio3</section>
-    <section>Contac</section>
+    <section id="Services"><Parallax type="services"/></section>
+    <section>Services</section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contac">Contac</section>
     {/*<Test/>
     <Test/>*/}
   </div>;
